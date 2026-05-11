6dca0b70ce3606c54696f697c9928dcb86feae37
madhumitha <madhumitha@10.22.207.182>
2026-05-11 22:06:17 +0530

    updated framework

--- com.unified.framework/src/main/java/ObjectRepository/LoginPage.java
+++ com.unified.framework/src/main/java/ObjectRepository/LoginPage.java
@@ -1,9 +1,12 @@
 package ObjectRepository;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 
 public class LoginPage {
 
@@ -13,4 +16,18 @@ public class LoginPage {
 		this.driver=driver;
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
+	
+	@AndroidFindBy(xpath = "//android.view.View[@content-desc=\"Skip\"]")
+	@iOSXCUITFindBy()
+	private WebElement skipBtn;
+	
+	
+	@AndroidFindBy(className = "android.widget.EditText")
+	@iOSXCUITFindBy()
+	private WebElement mobileNumber;
+	
+	@AndroidFindBy(id = "getOtpBtn")
+	@iOSXCUITFindBy
+	private WebElement getOTPBtn;
+
 }
